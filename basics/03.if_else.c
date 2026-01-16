a86f97edd70c27945f9ad364196caaac498e8478
lohotshreya <lohotshreya@gmail.com>
2026-01-16 11:03:55 +0530

    Add if-else example

---
+++ basics/03.if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 10;
+
+    if (number > 0) {
+        printf("Positive number\n");
+    } else {
+        printf("Not a positive number\n");
+    }
+
+    return 0;
+}
