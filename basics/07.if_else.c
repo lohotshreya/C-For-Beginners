6428a337bb035ffe089103d879a994119ab23859
lohotshreya <lohotshreya@gmail.com>
2026-01-20 16:48:39 +0530

    Add if-else example for even and odd numbers

---
+++ basics/07.if_else.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 7;
+
+    if (number % 2 == 0) {
+        printf("Even number\n");
+    } else {
+        printf("Odd number\n");
+    }
+
+    return 0;
+}
